f9915d18d44ff431fc29e81e691671b42e50aa28
HP <HP@LAPTOP-7CM41USF>
2022-04-22 08:20:28 +0530

    commit the changes

--- src/test/java/demoAlert/AlertDemo.java
+++ src/test/java/demoAlert/AlertDemo.java
@@ -12,7 +12,7 @@ static WebDriver driver;
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sony\\git\\PracticeDemo\\MavenDemSelArtifact\\src\\test\\resources\\driver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\eclipse-workspace\\SeleniumMarchBatchProj\\Drivers\\chromedriver.exe");
 		driver=new ChromeDriver();//Launch the Chrome Browser
 		Thread.sleep(5000);
 		System.out.println("Browser launched");
@@ -23,18 +23,20 @@ static WebDriver driver;
 	driver.findElement(By.id("alertBox")).click();
 	if(ExpectedConditions.alertIsPresent()!=null)
 	{
-		
+//		Thread.sleep(2000);
 		Alert a=driver.switchTo().alert();
-		String alertmsg=a.getText();
-		Thread.sleep(1000);
-		System.out.println("Allert Msg is "+alertmsg);
-		Assert.assertEquals(alertmsg, "I am an alert box!");
 		a.accept();
-		Thread.sleep(1000);
-		String outputmsg=driver.findElement(By.id("output")).getText();
-		Thread.sleep(1000);
-		System.out.println("Allert Msg is "+outputmsg);
-		Assert.assertEquals(outputmsg, "You selected alert popup");
+		driver.close();
+//		String alertmsg=a.getText();
+//		Thread.sleep(1000);
+//		System.out.println("Allert Msg is "+alertmsg);
+//		Assert.assertEquals(alertmsg, "I am an alert box!");
+//		a.accept();
+//		Thread.sleep(1000);
+//		String outputmsg=driver.findElement(By.id("output")).getText();
+//		Thread.sleep(1000);
+//		System.out.println("Allert Msg is "+outputmsg);
+//		Assert.assertEquals(outputmsg, "You selected alert popup");
 		
 	}
 
